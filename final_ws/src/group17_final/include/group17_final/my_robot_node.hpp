f3d7e5f5b87e00a2a49f4aefe91b08226c9ef4e4
Keyur Borad <keyurborad5@gmail.com>
2023-12-15 20:28:56 -0500

    Subscribed .Broadcasted and listened all part frame
    Created seperate subsciption for all the parts
    created cseperate callback function for all the parts
    listened the transformed frame once in the callback for this I have used reset the pointer of subscription.

--- final_ws/src/group17_final/include/group17_final/my_robot_node.hpp
+++ final_ws/src/group17_final/include/group17_final/my_robot_node.hpp
@@ -16,25 +16,92 @@ class MyRobotNode : public rclcpp::Node{
     public:
      MyRobotNode(std::string node_name) : Node(node_name){
 
-         //********************Subscriber**************************
+        //************************Broadcaster******************************
+        // initialize the transform broadcaster
+        part_tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+        // Load a buffer of transforms
+        part_tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        part_tf_buffer_->setUsingDedicatedThread(true);
+         
+
+
+        //************************Listener******************************
+        // load a buffer of transforms
+        part_tf_listener_buffer_ =std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        part_tf_listener_buffer_->setUsingDedicatedThread(true);
+        part_transform_listener_ =std::make_shared<tf2_ros::TransformListener>(*part_tf_listener_buffer_);
+
+
+        //********************Subscriber**************************
         
         aruco_cam_subscriber_=this->create_subscription<ros2_aruco_interfaces::msg::ArucoMarkers>("aruco_markers",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::aruco_cam_sub_cb,this,std::placeholders::_1));
         
         rclcpp::QoS qos(10); qos.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
-        //part_cam_subscriber_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/advanced_logical_camera/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb,this,std::placeholders::_1));
+        // for(int i=0;i<3;i++){
+        //     camera_frame_=advanced_camera_frames_.at(i);
+        //     RCLCPP_INFO_STREAM(this->get_logger(),"INside of : "<<camera_frame_.c_str()); 
+        //     part_cam_subscriber_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>(advanced_camera_topics_.at(i).c_str(),rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb,this,std::placeholders::_1));
         
+        // }
+
+        part_cam_subscriber1_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera1/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb1,this,std::placeholders::_1));
+        part_cam_subscriber2_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera2/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb2,this,std::placeholders::_1));
+        part_cam_subscriber3_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera3/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb3,this,std::placeholders::_1));
+        part_cam_subscriber4_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera4/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb4,this,std::placeholders::_1));
+        part_cam_subscriber5_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera5/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb5,this,std::placeholders::_1));
+
 
      }
 
      private:
-     //###########################-----------SUBSCRIBER------------########################
 
-     //******************Attribubtes*******************
+    //###########################-----------BROADCASTER------------########################
+
+    //******************Broadcaster Attribubtes*******************
+
+    /*!< Buffer that stores several seconds of transforms for easy lookup by the listener. */
+    std::shared_ptr<tf2_ros::Buffer> part_tf_buffer_;
+    /*!< MyRobotNode object */
+    std::shared_ptr<tf2_ros::TransformBroadcaster> part_tf_broadcaster_;
+
+    //**********************Broadcaster Methods***********************
+    /**
+     * @brief Timer to broadcast the transform
+     *
+     */
+    void part_broadcaster(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr);
+
+    //###########################-----------LISTENER------------########################
+
+     //******************Listener Attribubtes*******************
+    std::unique_ptr<tf2_ros::Buffer> part_tf_listener_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> part_transform_listener_{nullptr};
+    
+    //**********************Listener Methods***********************
+    /**
+     * @brief Listen to a part transform
+     *
+     * @param source_frame Source frame (child frame) of the transform
+     * @param target_frame Target frame (parent frame) of the transform
+     */
+    void part_listen_transform(const std::string &source_frame, const std::string &target_frame);
+
+    
+
+    
+    //###########################-----------SUBSCRIBER------------########################
+
+    //******************Subscriber Attribubtes*******************
 
     rclcpp::Subscription<ros2_aruco_interfaces::msg::ArucoMarkers>::SharedPtr aruco_cam_subscriber_;
-    rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber_;
+    rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber1_;
+    rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber2_;
+    rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber3_;
+    rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber4_;
+    rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber5_;
 
-    //**********************Methods***********************
+
+    //**********************Subscriber Methods***********************
     /**
      * @brief Timer callback for Aruco camera to continously read the feed
      * 
@@ -46,13 +113,22 @@ class MyRobotNode : public rclcpp::Node{
      * 
      * @param msg A type of message sent by the TOPIC mage/Advanced_logical_camera/Images
      */
-    void part_cam_sub_cb(const mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg);
+    void part_cam_sub_cb1(const mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg);
+    void part_cam_sub_cb2(const mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg);
+    void part_cam_sub_cb3(const mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg);
+    void part_cam_sub_cb4(const mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg);
+    void part_cam_sub_cb5(const mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg);
+
 
     //###########################-----------ADDITIONAL METHODS------------########################
 
     //###########################-----------ADDITIONAL ATTRIBUTES------------########################
     int marker_id_;
     bool found_marker_id_=false;
+    int part_color_;
+    std::vector<std::string> advanced_camera_topics_{"mage/camera1/image","mage/camera2/image","mage/camera3/image","mage/camera4/image","mage/camera5/image"};
+    std::vector<std::string> advanced_camera_frames_{"camera1_frame","camera2_frame","camera3_frame","camera4_frame","camera5_frame"};
+    std::string camera_frame_;
 
 
 };
--- final_ws/src/group17_final/src/my_robot_node.cpp
+++ final_ws/src/group17_final/src/my_robot_node.cpp
@@ -7,6 +7,79 @@
 #include <geometry_msgs/msg/pose.hpp>
 
 
+//A broadcaster to assign a frame for the detected object
+void MyRobotNode::part_broadcaster(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg)
+{
+    geometry_msgs::msg::TransformStamped part_dynamic_transform_stamped;
+
+    // RCLCPP_INFO(this->get_logger(), "Broadcasting dynamic_frame");
+    part_dynamic_transform_stamped.header.stamp = this->get_clock()->now();
+
+    part_dynamic_transform_stamped.header.frame_id = camera_frame_.c_str();
+    part_dynamic_transform_stamped.child_frame_id = "part";
+
+    part_color_= msg->part_poses[0].part.color;
+    //part_type_= msg->part_poses[0].part.type;
+
+    part_dynamic_transform_stamped.transform.translation.x = msg->part_poses[0].pose.position.x;
+    part_dynamic_transform_stamped.transform.translation.y = msg->part_poses[0].pose.position.y;
+    part_dynamic_transform_stamped.transform.translation.z = -msg->part_poses[0].pose.position.z;//had to make it neg because it was detecting object below the floor
+
+    part_dynamic_transform_stamped.transform.rotation.x = msg->part_poses[0].pose.orientation.x;
+    part_dynamic_transform_stamped.transform.rotation.y = msg->part_poses[0].pose.orientation.y;
+    part_dynamic_transform_stamped.transform.rotation.z = msg->part_poses[0].pose.orientation.z;
+    part_dynamic_transform_stamped.transform.rotation.w = msg->part_poses[0].pose.orientation.w;
+    // Send the transform
+    part_tf_broadcaster_->sendTransform(part_dynamic_transform_stamped);
+    
+    // RCLCPP_INFO_STREAM(this->get_logger(), "PART advance cam Broadcasting_dynamic_frame : "<<msg->part_poses[0].pose.position.x;);
+}
+
+// A listerner that would tansform a detected part in odom frame
+void MyRobotNode::part_listen_transform(const std::string &source_frame, const std::string &target_frame)
+{
+    geometry_msgs::msg::TransformStamped t_stamped;
+    geometry_msgs::msg::Pose pose_out;
+    try
+    {
+        t_stamped = part_tf_listener_buffer_->lookupTransform(source_frame, target_frame, tf2::TimePointZero, 50ms);
+    }
+    catch (const tf2::TransformException &ex)
+    {
+        RCLCPP_ERROR_STREAM(this->get_logger(), "Could not get transform between " << source_frame << " and " << target_frame << ": " << ex.what());
+        return;
+    }
+
+    pose_out.position.x = t_stamped.transform.translation.x;
+    pose_out.position.y = t_stamped.transform.translation.y;
+    pose_out.position.z = t_stamped.transform.translation.z;
+    pose_out.orientation = t_stamped.transform.rotation;
+    RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART COLOR: "<<part_color_);
+
+    // //converting detected geometery quaternion to tf2 quaternion
+    // tf2::Quaternion q( 
+    //     pose_out.orientation.x,
+    //     pose_out.orientation.y,
+    //     pose_out.orientation.z,
+    //     pose_out.orientation.w);
+    // //Utlising Utlis to convert quaternion to rpy
+    // std::array<double, 3> euler = utils_ptr_->set_euler_from_quaternion(q);
+    // //initialising local variable as vector of data of detected object
+    // std::vector<double> detection={part_color_,pose_out.position.x,pose_out.position.y,pose_out.position.z,euler[0],euler[1],euler[2]};
+    // //passing the above vector to logg all the detected parts
+    // detected_part_locations(detection);
+    
+
+    // RCLCPP_INFO_STREAM(this->get_logger(), target_frame << " in " << source_frame << ":\n"
+    //                                                     << "x: " << pose_out.position.x << "\t"
+    //                                                     << "y: " << pose_out.position.y << "\t"
+    //                                                     << "z: " << pose_out.position.z << "\n"
+    //                                                     << "qx: " << pose_out.orientation.x << "\t"
+    //                                                     << "qy: " << pose_out.orientation.y << "\t"
+    //                                                     << "qz: " << pose_out.orientation.z << "\t"
+    //                                                     << "qw: " << pose_out.orientation.w << "\n");
+}
+
 //Aruco camera callback method
 void MyRobotNode::aruco_cam_sub_cb(ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr msg){
 
@@ -21,6 +94,125 @@ void MyRobotNode::aruco_cam_sub_cb(ros2_aruco_interfaces::msg::ArucoMarkers::Sha
     }
 
 }
+
+//Part's Advanced logical camera callback method
+void MyRobotNode::part_cam_sub_cb1(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg){
+    
+    if(!msg->part_poses.empty()){//Very necesaary otherwise node will die by it self; checking if topic is publishing or not
+    
+    //RCLCPP_INFO_STREAM(this->get_logger(),"Part Advance Camera callback : ");
+
+    //once detected part, then we can broadcast a new frame at the detected location 
+    camera_frame_="camera1_frame";
+    part_broadcaster(msg);
+    
+    //after broadcasting we can listen the part frame wrt to any frame ,here we want wrt odom frame
+    part_listen_transform("map", "part");
+         
+    RCLCPP_INFO_STREAM(this->get_logger(),"Continue Part subscription camera 1: ");
+
+    part_cam_subscriber1_.reset(); 
+        
+        }
+    else{    //RCLCPP_INFO_STREAM(this->get_logger(),"NO Part Detected : ");
+    }
+
+}
+//Part's Advanced logical camera callback method
+void MyRobotNode::part_cam_sub_cb2(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg){
+    
+    if(!msg->part_poses.empty()){//Very necesaary otherwise node will die by it self; checking if topic is publishing or not
+    
+    //RCLCPP_INFO_STREAM(this->get_logger(),"Part Advance Camera callback : ");
+
+    //once detected part, then we can broadcast a new frame at the detected location 
+    camera_frame_="camera2_frame";
+    part_broadcaster(msg);
+    //after broadcasting we can listen the part frame wrt to any frame ,here we want wrt odom frame
+    part_listen_transform("map", "part");
+
+         
+    RCLCPP_INFO_STREAM(this->get_logger(),"Continue Part subscription camera 2: ");
+
+    part_cam_subscriber2_.reset(); 
+        
+        }
+    else{    //RCLCPP_INFO_STREAM(this->get_logger(),"NO Part Detected : ");
+    }
+
+}
+
+//Part's Advanced logical camera callback method
+void MyRobotNode::part_cam_sub_cb3(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg){
+    
+    if(!msg->part_poses.empty()){//Very necesaary otherwise node will die by it self; checking if topic is publishing or not
+    
+    //RCLCPP_INFO_STREAM(this->get_logger(),"Part Advance Camera callback : ");
+
+    //once detected part, then we can broadcast a new frame at the detected location 
+    camera_frame_="camera3_frame";
+    part_broadcaster(msg);
+    //after broadcasting we can listen the part frame wrt to any frame ,here we want wrt odom frame
+    part_listen_transform("map", "part");
+
+         
+    RCLCPP_INFO_STREAM(this->get_logger(),"Continue Part subscription camera 3: ");
+
+    part_cam_subscriber3_.reset(); 
+        
+        }
+    else{    //RCLCPP_INFO_STREAM(this->get_logger(),"NO Part Detected : ");
+    }
+
+}
+
+void MyRobotNode::part_cam_sub_cb4(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg){
+    
+    if(!msg->part_poses.empty()){//Very necesaary otherwise node will die by it self; checking if topic is publishing or not
+    
+    //RCLCPP_INFO_STREAM(this->get_logger(),"Part Advance Camera callback : ");
+
+    //once detected part, then we can broadcast a new frame at the detected location 
+    camera_frame_="camera4_frame";
+    part_broadcaster(msg);
+    //after broadcasting we can listen the part frame wrt to any frame ,here we want wrt odom frame
+    part_listen_transform("map", "part");
+
+         
+    RCLCPP_INFO_STREAM(this->get_logger(),"Continue Part subscription camera 4: ");
+
+    part_cam_subscriber4_.reset(); 
+        
+        }
+    else{    //RCLCPP_INFO_STREAM(this->get_logger(),"NO Part Detected : ");
+    }
+
+}
+
+void MyRobotNode::part_cam_sub_cb5(mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg){
+    
+    if(!msg->part_poses.empty()){//Very necesaary otherwise node will die by it self; checking if topic is publishing or not
+    
+    //RCLCPP_INFO_STREAM(this->get_logger(),"Part Advance Camera callback : ");
+
+    //once detected part, then we can broadcast a new frame at the detected location 
+    camera_frame_="camera5_frame";
+    part_broadcaster(msg);
+    //after broadcasting we can listen the part frame wrt to any frame ,here we want wrt odom frame
+    part_listen_transform("map", "part");
+
+         
+    RCLCPP_INFO_STREAM(this->get_logger(),"Continue Part subscription camera 5: ");
+
+    part_cam_subscriber5_.reset(); 
+        
+        }
+    else{    //RCLCPP_INFO_STREAM(this->get_logger(),"NO Part Detected : ");
+    }
+
+}
+
+
 
 
 
