a8e88064fc09861818b1edfbcb232a7e9d8bfecb
Keyur Borad <keyurborad5@gmail.com>
2023-12-17 02:47:05 -0500

    Implemented NavigationThroughPoses
    Implemented NavigationThroughPoses

--- final_ws/install/group17_final/share/group17_final/include/group17_final/my_robot_node.hpp
+++ final_ws/install/group17_final/share/group17_final/include/group17_final/my_robot_node.hpp
@@ -3,17 +3,23 @@
 #include <cmath>
 #include <geometry_msgs/msg/pose.hpp>
 #include<nav_msgs/msg/odometry.hpp>
+//#include "nav2_msgs/action/navigate_to_pose.hpp"
+#include "nav2_msgs/action/navigate_through_poses.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 // for dynamic broadcaster
 #include "tf2_ros/transform_broadcaster.h"
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
 #include <ros2_aruco_interfaces/msg/aruco_markers.hpp>
 #include <mage_msgs/msg/advanced_logical_camera_image.hpp>
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 using namespace std::chrono_literals;
 
 class MyRobotNode : public rclcpp::Node{
 
     public:
+    using NavigateThroughPoses = nav2_msgs::action::NavigateThroughPoses;
+    using GoalHandleNavigation = rclcpp_action::ClientGoalHandle<NavigateThroughPoses>;
      MyRobotNode(std::string node_name) : Node(node_name){
 
         std::vector<std::string>wp{"wp1","wp2","wp3","wp4","wp5"};
@@ -46,6 +52,11 @@ class MyRobotNode : public rclcpp::Node{
         part_transform_listener_ =std::make_shared<tf2_ros::TransformListener>(*part_tf_listener_buffer_);
 
 
+        //********************Publisher**************************
+
+        // initialize the publisher
+        initial_pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("initialpose", 10);
+
         //********************Subscriber**************************
         
         // aruco_cam_subscriber_=this->create_subscription<ros2_aruco_interfaces::msg::ArucoMarkers>("aruco_markers",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::aruco_cam_sub_cb,this,std::placeholders::_1));
@@ -53,16 +64,56 @@ class MyRobotNode : public rclcpp::Node{
         rclcpp::QoS qos(10); qos.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
         
 
-        part_cam_subscriber1_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera1/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb1,this,std::placeholders::_1));
-        part_cam_subscriber2_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera2/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb2,this,std::placeholders::_1));
-        part_cam_subscriber3_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera3/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb3,this,std::placeholders::_1));
-        part_cam_subscriber4_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera4/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb4,this,std::placeholders::_1));
-        part_cam_subscriber5_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera5/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb5,this,std::placeholders::_1));
+        // part_cam_subscriber1_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera1/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb1,this,std::placeholders::_1));
+        // part_cam_subscriber2_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera2/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb2,this,std::placeholders::_1));
+        // part_cam_subscriber3_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera3/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb3,this,std::placeholders::_1));
+        // part_cam_subscriber4_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera4/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb4,this,std::placeholders::_1));
+        // part_cam_subscriber5_=this->create_subscription<mage_msgs::msg::AdvancedLogicalCameraImage>("mage/camera5/image",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::part_cam_sub_cb5,this,std::placeholders::_1));
+        odom_subscriber_=this->create_subscription<nav_msgs::msg::Odometry>("odom",rclcpp::SensorDataQoS(), std::bind(&MyRobotNode::odom_sub_cb,this,std::placeholders::_1));
 
+        //************************Action******************************
+        // initialize the client
+        client_ = rclcpp_action::create_client<NavigateThroughPoses>(this, "navigate_through_poses");
+        // // pause for 5 seconds
+        // std::this_thread::sleep_for(std::chrono::seconds(15));
+        // // send the goal
+        // send_goal();
 
      }
 
      private:
+
+    //###########################-----------ACTION SERVER------------########################
+
+    //******************Action Attribubtes*******************
+    /**
+     * @brief Action client for the action server navigate_to_pose
+     *
+     */
+    rclcpp_action::Client<NavigateThroughPoses>::SharedPtr client_;
+    //   rclcpp::TimerBase::SharedPtr timer_;
+
+    //******************Action Methods*******************
+    /**
+     * @brief Response from the server after sending the goal
+     */
+    void goal_response_callback(
+        std::shared_future<GoalHandleNavigation::SharedPtr> future);
+    /**
+     * @brief Feedback received while the robot is driving towards the goal
+     *
+     * @param feedback
+     */
+    void feedback_callback(
+        GoalHandleNavigation::SharedPtr,
+        const std::shared_ptr<const NavigateThroughPoses::Feedback> feedback);
+    /**
+     * @brief Result after the action has completed
+     *
+     * @param result
+     */
+    void result_callback(const GoalHandleNavigation::WrappedResult& result);
+
 
     //###########################-----------BROADCASTER------------########################
 
@@ -95,9 +146,20 @@ class MyRobotNode : public rclcpp::Node{
      */
     void part_listen_transform(const std::string &source_frame, const std::string &target_frame);
 
+    //###########################-----------PUBLISHER------------########################
+
+    //******************Publisher Attribubtes*******************  
+
+    /**
+     * @brief Publisher to the topic /initialpose
+    *
+    */
+    rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr initial_pose_pub_;
+
+    //**********************Publisher Methods***********************
     
 
-    
+
     //###########################-----------SUBSCRIBER------------########################
 
     //******************Subscriber Attribubtes*******************
@@ -108,6 +170,9 @@ class MyRobotNode : public rclcpp::Node{
     rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber3_;
     rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber4_;
     rclcpp::Subscription<mage_msgs::msg::AdvancedLogicalCameraImage>::SharedPtr part_cam_subscriber5_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber_;
+
+
 
 
     //**********************Subscriber Methods***********************
@@ -128,18 +193,29 @@ class MyRobotNode : public rclcpp::Node{
     void part_cam_sub_cb4(const mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg);
     void part_cam_sub_cb5(const mage_msgs::msg::AdvancedLogicalCameraImage::SharedPtr msg);
 
+     /**
+     * @brief A Timer Callback for reading the Odometry data from Topic odom
+     * 
+     * @param msg A type of message sent by the Topic /odom
+     */
+    void odom_sub_cb(nav_msgs::msg::Odometry::SharedPtr msg);
+
 
     //###########################-----------ADDITIONAL METHODS------------########################
-
+    // set the initial pose for navigation
+    void set_initial_pose(nav_msgs::msg::Odometry::SharedPtr msg);
+    void send_goal();
+    geometry_msgs::msg::PoseStamped createPose(double x, double y, double z);
     //###########################-----------ADDITIONAL ATTRIBUTES------------########################
     int marker_id_;
     bool found_marker_id_=false;
-    int part_color_;
+    double part_color_;
     std::vector<std::string> aruco_0_waypoints_;
     std::vector<std::string> aruco_1_waypoints_;
     std::vector<std::string> advanced_camera_topics_{"mage/camera1/image","mage/camera2/image","mage/camera3/image","mage/camera4/image","mage/camera5/image"};
     std::vector<std::string> advanced_camera_frames_{"camera1_frame","camera2_frame","camera3_frame","camera4_frame","camera5_frame"};
     std::string camera_frame_;
+    std::vector<std::vector<double>> parts_vector_;
 
 
 };
--- final_ws/src/group17_final/src/my_robot_node.cpp
+++ final_ws/src/group17_final/src/my_robot_node.cpp
@@ -18,38 +18,38 @@ void MyRobotNode::part_broadcaster(mage_msgs::msg::AdvancedLogicalCameraImage::S
     part_dynamic_transform_stamped.header.frame_id = camera_frame_.c_str();
     part_dynamic_transform_stamped.child_frame_id = "part";
 
-    part_color_= msg->part_poses[0].part.color;
+    part_color_= msg->part_poses.at(0).part.color;
     //part_type_= msg->part_poses[0].part.type;
 
-    switch(part_color_){
-        case mage_msgs::msg::Part::BLUE:
-        RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART BLUE COLOR: ");
-        break;
-        case mage_msgs::msg::Part::RED:
-        RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART RED COLOR: ");
-        break;
-        case mage_msgs::msg::Part::GREEN:
-        RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART GREEN COLOR: ");
-        break;
-        case mage_msgs::msg::Part::PURPLE:
-        RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART PURPLE COLOR: ");
-        break;
-        case mage_msgs::msg::Part::ORANGE:
-        RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART ORANGE COLOR: ");
-        break;
-        RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED NO COLOR: ");
-        default:
-        break;
-    }
+    // switch(part_color_){
+    //     case mage_msgs::msg::Part::BLUE:
+    //     RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART BLUE COLOR: ");
+    //     break;
+    //     case mage_msgs::msg::Part::RED:
+    //     RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART RED COLOR: ");
+    //     break;
+    //     case mage_msgs::msg::Part::GREEN:
+    //     RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART GREEN COLOR: ");
+    //     break;
+    //     case mage_msgs::msg::Part::PURPLE:
+    //     RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART PURPLE COLOR: ");
+    //     break;
+    //     case mage_msgs::msg::Part::ORANGE:
+    //     RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED PART ORANGE COLOR: ");
+    //     break;
+    //     RCLCPP_INFO_STREAM(this->get_logger(), "DETECTED NO COLOR: ");
+    //     default:
+    //     break;
+    // }
 
-    part_dynamic_transform_stamped.transform.translation.x = msg->part_poses[0].pose.position.x;
-    part_dynamic_transform_stamped.transform.translation.y = msg->part_poses[0].pose.position.y;
-    part_dynamic_transform_stamped.transform.translation.z = -msg->part_poses[0].pose.position.z;//had to make it neg because it was detecting object below the floor
+    part_dynamic_transform_stamped.transform.translation.x = msg->part_poses.at(0).pose.position.x;
+    part_dynamic_transform_stamped.transform.translation.y = msg->part_poses.at(0).pose.position.y;
+    part_dynamic_transform_stamped.transform.translation.z = -msg->part_poses.at(0).pose.position.z;//had to make it neg because it was detecting object below the floor
 
-    part_dynamic_transform_stamped.transform.rotation.x = msg->part_poses[0].pose.orientation.x;
-    part_dynamic_transform_stamped.transform.rotation.y = msg->part_poses[0].pose.orientation.y;
-    part_dynamic_transform_stamped.transform.rotation.z = msg->part_poses[0].pose.orientation.z;
-    part_dynamic_transform_stamped.transform.rotation.w = msg->part_poses[0].pose.orientation.w;
+    part_dynamic_transform_stamped.transform.rotation.x = msg->part_poses.at(0).pose.orientation.x;
+    part_dynamic_transform_stamped.transform.rotation.y = msg->part_poses.at(0).pose.orientation.y;
+    part_dynamic_transform_stamped.transform.rotation.z = msg->part_poses.at(0).pose.orientation.z;
+    part_dynamic_transform_stamped.transform.rotation.w = msg->part_poses.at(0).pose.orientation.w;
    
     // Send the transform
     part_tf_broadcaster_->sendTransform(part_dynamic_transform_stamped);
@@ -57,7 +57,7 @@ void MyRobotNode::part_broadcaster(mage_msgs::msg::AdvancedLogicalCameraImage::S
     // RCLCPP_INFO_STREAM(this->get_logger(), "PART advance cam Broadcasting_dynamic_frame : "<<msg->part_poses[0].pose.position.x;);
 }
 
-// A listerner that would tansform a detected part in odom frame
+// A listerner that would tansform a detected part in map frame
 void MyRobotNode::part_listen_transform(const std::string &source_frame, const std::string &target_frame)
 {
     geometry_msgs::msg::TransformStamped t_stamped;
@@ -86,8 +86,8 @@ void MyRobotNode::part_listen_transform(const std::string &source_frame, const s
     //     pose_out.orientation.w);
     // //Utlising Utlis to convert quaternion to rpy
     // std::array<double, 3> euler = utils_ptr_->set_euler_from_quaternion(q);
-    //initialising local variable as vector of data of detected object
-     std::vector<std::variant<int,double> >detection={part_color_,pose_out.position.x,
+    //initialising local variable as vector of data of detected object //std::variant<int,double>
+     std::vector<double>detection={part_color_,pose_out.position.x,
                                                 pose_out.position.y,
                                                 pose_out.position.z,
                                                 pose_out.orientation.x,
@@ -99,12 +99,139 @@ void MyRobotNode::part_listen_transform(const std::string &source_frame, const s
     
 }
 
+//Callback method to get the location and orientaton of the robot
+void MyRobotNode::odom_sub_cb(nav_msgs::msg::Odometry::SharedPtr msg){
+
+    // for (const auto& vect : parts_vector_) {
+    //  RCLCPP_INFO_STREAM(this->get_logger(),"Another part ");
+
+    //         for ( const auto element : vect) {
+    //             RCLCPP_INFO_STREAM(this->get_logger(),  element << ' ');
+    //         }
+            
+    //     }
+    set_initial_pose(msg);
+    // pause for 5 seconds
+        std::this_thread::sleep_for(std::chrono::seconds(15));
+        // send the goal
+        send_goal();
+
+    odom_subscriber_.reset();
+    // tf2::Quaternion q( 
+    //     msg->pose.pose.orientation.x,
+    //     msg->pose.pose.orientation.y,
+    //     msg->pose.pose.orientation.z,
+    //     msg->pose.pose.orientation.w);
+    // //converting quaternion to euler rpy
+    // std::array<double, 3> euler = utils_ptr_->set_euler_from_quaternion(q);
+    // //RCLCPP_INFO_STREAM(this->get_logger(), "Euler angles : "<< euler[0]<<","<<euler[1]<<","<<euler[2]<<"\n");
+    // //Assigning Z rotation value to a attribute that would be used to make turns
+    // current_yaw_=euler[2];
+}
+
+//======Initial Pose=========================================
+void MyRobotNode::set_initial_pose(nav_msgs::msg::Odometry::SharedPtr msg) {
+  auto message = geometry_msgs::msg::PoseWithCovarianceStamped();
+  message.header.frame_id = "map";
+  message.pose.pose.position.x = msg->pose.pose.position.x;
+  message.pose.pose.position.y = msg->pose.pose.position.y;
+  message.pose.pose.orientation.x = msg->pose.pose.orientation.x;
+  message.pose.pose.orientation.y = msg->pose.pose.orientation.y;
+  message.pose.pose.orientation.z = msg->pose.pose.orientation.z;
+  message.pose.pose.orientation.w = msg->pose.pose.orientation.w;
+  initial_pose_pub_->publish(message);
+}
+
+geometry_msgs::msg::PoseStamped MyRobotNode::createPose(double x, double y, double z){
+    auto pose_stamped = geometry_msgs::msg::PoseStamped();
+    pose_stamped.pose.position.x = x;
+    pose_stamped.pose.position.y = y;
+    pose_stamped.pose.position.z = z;
+    pose_stamped.header.stamp = this->get_clock()->now();
+    pose_stamped.header.frame_id = "map";  // Set the appropriate frame ID
+    return pose_stamped;
+} 
+//==========SEND GOAL===================================
+void MyRobotNode::send_goal() {
+  using namespace std::placeholders;
+
+  if (!this->client_->wait_for_action_server()) {
+    RCLCPP_ERROR(this->get_logger(),
+                 "Action server not available after waiting");
+    rclcpp::shutdown();
+  }
+
+  auto goal_msg = NavigateThroughPoses::Goal();
+  goal_msg.poses.push_back(createPose(1.9, -2.5, 1.0));
+  goal_msg.poses.push_back(createPose(6.23, -2.35, 1.0));
+  //goal_msg.poses.push_back(createPose(6.43, 2.05, 1.0));
+//   goal_msg.poses.push_back(createPose(4.26, 0.469, 1.0));
+//   goal_msg.poses.push_back(createPose(1.6, 2.5, 1.0));
+//                                                                                                             // goal_msg.pose.header.frame_id = "map";
+                                                                                                            // goal_msg.pose.pose.position.x = 1.5;
+                                                                                                            // goal_msg.pose.pose.position.y = 0.0;
+                                                                                                            // goal_msg.pose.pose.position.z = 0.0;
+                                                                                                            // goal_msg.pose.pose.orientation.x = 0.0;
+                                                                                                            // goal_msg.pose.pose.orientation.y = 0.0;
+                                                                                                            // goal_msg.pose.pose.orientation.z = 0.0;
+                                                                                                            // goal_msg.pose.pose.orientation.w = 0.7787;
+
+  RCLCPP_INFO(this->get_logger(), "Sending goal");
+
+  auto send_goal_options = rclcpp_action::Client<NavigateThroughPoses>::SendGoalOptions();
+  send_goal_options.goal_response_callback = std::bind(&MyRobotNode::goal_response_callback, this, _1);
+  // send_goal_options.feedback_callback = std::bind(&MyRobotNode::feedback_callback, this, _1, _2);
+  send_goal_options.result_callback = std::bind(&MyRobotNode::result_callback, this, _1);
+
+  client_->async_send_goal(goal_msg, send_goal_options);
+}
+
+//========Goal Response Callback=======================================
+void MyRobotNode::goal_response_callback(
+    std::shared_future<GoalHandleNavigation::SharedPtr> future) {
+    auto goal_handle = future.get();
+    if (!goal_handle) {
+        RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+        rclcpp::shutdown();
+    } 
+    else {
+        RCLCPP_INFO(this->get_logger(),"Goal accepted by server, waiting for result");
+    }
+}
+
+//=======Feedback Callback========================================
+// void MyRobotNode::feedback_callback(
+//     GoalHandleNavigation::SharedPtr,
+//     const std::shared_ptr<const NavigateThroughPoses::Feedback> feedback) {
+//   RCLCPP_INFO(this->get_logger(), "Robot is driving towards the goal");
+// }
+
+//=======Result Callback========================================
+void MyRobotNode::result_callback(
+    const GoalHandleNavigation::WrappedResult& result) {
+  switch (result.code) {
+    case rclcpp_action::ResultCode::SUCCEEDED:
+     RCLCPP_INFO(this->get_logger(), "Goal was Success");
+      break;
+    case rclcpp_action::ResultCode::ABORTED:
+      RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+      return;
+    case rclcpp_action::ResultCode::CANCELED:
+      RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+      return;
+    default:
+      RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+      return;
+  }
+  rclcpp::shutdown();
+}
+
 //Aruco camera callback method
 void MyRobotNode::aruco_cam_sub_cb(ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr msg){
 
    if(!msg->marker_ids.empty()){//to check if the topic is pusblishing null
-    marker_id_=msg->marker_ids[0];
-    RCLCPP_INFO_STREAM(this->get_logger(),"FOUND Aruco marker ID: "<<msg->marker_ids[0]);
+    marker_id_=msg->marker_ids.at(0);
+    RCLCPP_INFO_STREAM(this->get_logger(),"FOUND Aruco marker ID: "<<msg->marker_ids.at(0));
     aruco_cam_subscriber_.reset();
    }
     else{
